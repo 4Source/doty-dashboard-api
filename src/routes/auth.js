03d74ee1b36829fbaca722222613ca06ad8481cc
4Source <4Source@users.noreply.github.com>
2022-01-22 17:00:53 +0100

    Added /auth/logout to logout User

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,9 +7,17 @@ router.get('/', passport.authenticate('discord'));
 router.get('/redirect', passport.authenticate('discord', {
     failureRedirect: '/forbidden',
     successRedirect: '/dashboard'
-}), (req, res) => {
-    res.send(req.user);
-});
+}));
+
+router.get('/logout', (req, res) => {
+    if(req.user) {
+        req.logout();
+        res.redirect('/');
+    }
+    else {
+        res.redirect('/');
+    }
+})
 
 
 module.exports = router;
