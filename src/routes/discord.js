3a57373aba001ea27d9a57fddbccebd3596cfbb8
4Source <4Source@users.noreply.github.com>
2022-01-24 16:10:30 +0100

    FIX json to send

--- src/routes/discord.js
+++ src/routes/discord.js
@@ -2,7 +2,7 @@ const { getMutualGuilds } = require('../services/discord/discord.service');
 const router = require("express").Router();
 
 router.get('/guilds', async (req, res) => {
-    res.json(await getMutualGuilds(req.user.dataValues.accessToken));
+    res.send(await getMutualGuilds(req.user.dataValues.accessToken));
 });
 
 module.exports = router;
