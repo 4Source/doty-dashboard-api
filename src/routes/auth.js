38d056fb0bdbfa4cbac0c26d9966f7477e9462dd
4Source <4Source@users.noreply.github.com>
2022-01-25 17:23:48 +0100

    Remove accessToken/refreshToken for ClientSide

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,12 +12,18 @@ router.get('/redirect', passport.authenticate('discord', {
 
 // Status
 router.get('/status', async(req, res) => {
-    if(!req.user) res.status(404).send('User is NOT logged in.');
-    res.status(200).send(`User(${req.user.user_id}) is logged in.`);
+    if(!req.user) res.status(404).send(null);
+    else {
+        var user = req.user;
+        delete user.accessToken;
+        delete user.refreshToken;
+        res.status(200).send(user);
+    }
 });
 
 // Logout
 router.post('/logout', (req, res) => {
+    console.log("logout", req.user);
     if(req.user) {
         req.logout();
         res.redirect('/');
