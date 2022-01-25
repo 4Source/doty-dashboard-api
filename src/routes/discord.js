f8abafabbfcc711e060a841281b3135db3a9044d
4Source <4Source@users.noreply.github.com>
2022-01-25 17:26:04 +0100

    Fixed Updated DiscordStrategy

--- src/routes/discord.js
+++ src/routes/discord.js
@@ -2,7 +2,7 @@ const { getMutualGuilds } = require('../services/discord/discord.service');
 const router = require("express").Router();
 
 router.get('/guilds', async (req, res) => {
-    res.send(await getMutualGuilds(req.user.dataValues.accessToken));
+    res.send(await getMutualGuilds(req.user.accessToken));
 });
 
 module.exports = router;
